71d3c23c6662fe5bd654d5ab2f14d31754a50e95
Kyu Park <park@kyu.co>
2016-04-04 19:07:07 -0400

    js fix

--- web/shop.js
+++ web/shop.js
@@ -1,8 +1,6 @@
 $(document).ready(function(){
-	$(".item").find("review").text().hide();
-	$(".item").hover(function(){
-		$(this).find("review").text().show();
-		}, function(){
-		$(this).find("review").text().hide();
+	$(".review").hide();
+	$(".item").click(function(){
+	     $(this).find(".review").toggle();
 	});
-});
+}); 
