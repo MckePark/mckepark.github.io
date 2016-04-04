5248e63bbc068831276138c00370211347b7f863
Kyu Park <park@kyu.co>
2016-04-04 16:54:34 -0400

    this should work

--- web/shop.js
+++ web/shop.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
-$(".item").find("review").text().hide();
-$(".item").hover(function(){
-	$(this).find("review").text().show();
-	}, function(){
-	$(this).find("review").text().hide();
+	$(".item").find("review").text().hide();
+	$(".item").hover(function(){
+		$(this).find("review").text().show();
+		}, function(){
+		$(this).find("review").text().hide();
+	});
 });
