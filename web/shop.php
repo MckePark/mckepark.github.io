82d5f725ebe83c5d9a5fe369e28ccac07a890886
Kyu Park <park@kyu.co>
2016-04-07 09:18:11 -0400

    shop br tags

--- web/shop.php
+++ web/shop.php
@@ -78,8 +78,11 @@ while ($row = pg_fetch_row($result))
       <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
       <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
     </form>
+    <br>
     <img class="shop-img" id="img$id" src="$img">
+    <br>
     Customer Reviews: $review
+    <br>
   </div>
 
 HEREDOC;
